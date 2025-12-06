da564e735cfd06ba48450b98097c778551584dc8
hikma26 <hikma7091@gmail.com>
2025-12-06 09:31:55 +0700

    first commit

---
+++ app/src/main/java/com/example/c1_prak6_13020250206/RestCallbackMahasiswa.java
@@ -0,0 +1,7 @@
+package com.example.c1_prak6_13020250206;
+
+import java.util.ArrayList;
+
+public interface RestCallbackMahasiswa {
+    void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList);
+}
