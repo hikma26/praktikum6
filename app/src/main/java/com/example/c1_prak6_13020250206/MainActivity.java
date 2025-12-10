9122504fd286ac3b012774e079a15b0cecac0676
Nur Hikma <hikma7091@gmail.com>
2025-12-10 22:35:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/c1_prak6_13020250206/MainActivity.java
+++ app/src/main/java/com/example/c1_prak6_13020250206/MainActivity.java
@@ -72,3 +72,105 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+public class TampilDataActivity extends AppCompatActivity {
+
+    private TableLayout tblMhs;
+    private TableRow tr;
+    private TextView col1, col2, col3;
+    private RestHelper restHelper;
+    private String stb, nama, angkatan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_data);
+
+        restHelper = new RestHelper(this);
+        tblMhs = findViewById(R.id.tbl_mhs);
+
+        tampilData();
+    }
+
+    private void tampilTblMhs(ArrayList<Mahasiswa> arrListMhs) {
+        tblMhs.removeAllViews();
+
+        tr = new TableRow(this);
+        col1 = new TextView(this);
+        col2 = new TextView(this);
+        col3 = new TextView(this);
+
+        col1.setText("Stambuk");
+        col2.setText("Nama Mahasiswa");
+        col3.setText("Angkatan");
+
+        col1.setWidth(200);
+        col2.setWidth(300);
+        col3.setWidth(150);
+
+        tr.addView(col1);
+        tr.addView(col2);
+        tr.addView(col3);
+        tblMhs.addView(tr);
+
+        for (final Mahasiswa mhs : arrListMhs) {
+            tr = new TableRow(this);
+            col1 = new TextView(this);
+            col2 = new TextView(this);
+            col3 = new TextView(this);
+
+            col1.setText(mhs.getStb());
+            col2.setText(mhs.getNama());
+            col3.setText(String.valueOf(mhs.getAngkatan()));
+
+            col1.setWidth(200);
+            col2.setWidth(300);
+            col3.setWidth(150);
+
+            tr.addView(col1);
+            tr.addView(col2);
+            tr.addView(col3);
+
+            tblMhs.addView(tr);
+
+            tr.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    stb = mhs.getStb();
+                    nama = mhs.getNama();
+                    angkatan = String.valueOf(mhs.getAngkatan());
+                }
+            });
+        }
+    }
+
+    private void tampilData() {
+        restHelper.getDataMhs(new RestCallbackMahasiswa() {
+            @Override
+            public void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList) {
+                tampilTblMhs(arrayList);
+            }
+        });
+    }
+
+    public void btnEditClick(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("stb", stb);
+        intent.putExtra("nama", nama);
+        intent.putExtra("angkatan", angkatan);
+        setResult(1, intent);
+        finish();
+    }
+
+    public void btnHapusClick(View view) {
+        if (stb == null) return;
+
+        restHelper.hapusData(stb, new RestCallbackMahasiswa() {
+            @Override
+            public void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList) {
+                tampilTblMhs(arrayList);
+            }
+        });
+    }
+}
+
